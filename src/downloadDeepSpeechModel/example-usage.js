32443fe0cb1f0a077d121df59f65e5396edbf03d
Pietro Passarelli <pietro.passarelli@wsj.com>
2019-12-16 14:40:42 +0100

    added an npmignore

--- src/downloadDeepSpeechModel/example-usage.js
+++ src/downloadDeepSpeechModel/example-usage.js
@@ -1,5 +1,6 @@
 const downloadDeepSpeechModel = require('./index.js');
 const outputPath = 'models';
+
 downloadDeepSpeechModel(outputPath, '0.6.0').then((res)=>{
     console.log('res',res)
 }).catch((error)=>{
