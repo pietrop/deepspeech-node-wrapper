134bb75d170ebb632143a48e157460487916a3ad
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-04-12 23:26:04 -0400

    1.0.11
    rounding up words to 2 dec

--- src/convertDeepSpeechTimedCharToTimedWord/index.js
+++ src/convertDeepSpeechTimedCharToTimedWord/index.js
@@ -1,14 +1,26 @@
 const convertDeepSpeechTimedCharToTimedWord = require('./generateWords/index.js');
 const createParagraphsFromWords = require('./generateParagraphs/index.js');
 
+const roundUpWords = (words) => {
+  return words.map(({ start, end, text }) => {
+    return {
+      start: parseFloat(parseFloat(start).toFixed(2)),
+      end: parseFloat(parseFloat(end).toFixed(2)),
+      text,
+    };
+  });
+};
+
 /**
  *
  * @param {json} deepSpeechData - STT with char level timings from DeepSpeech `sttWithMetadata`
  */
+
 function convertDeepSpeechToDPE(deepSpeechData) {
   // TODO: test with something longer see if the alternative are need or just using first is ok
   const items = deepSpeechData.transcripts[0];
-  const words = convertDeepSpeechTimedCharToTimedWord(items.tokens);
+  const wordsRaw = convertDeepSpeechTimedCharToTimedWord(items.tokens);
+  const words = roundUpWords(wordsRaw);
   const paragraphs = createParagraphsFromWords(words);
   return { words, paragraphs };
 }
